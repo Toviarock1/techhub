e4afa7f12a8550b89f1d087b2da140a652ccdf81
simon adama <simontoviaadama@gmail.com>
2022-07-29 11:51:27 +0100

    feat: animate loading and error text

--- src/views/Programs/OurProgram/OurProgram.tsx
+++ src/views/Programs/OurProgram/OurProgram.tsx
@@ -1,8 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
+//framer-motion
+import { motion } from "framer-motion";
+//components
 import ProgramsCard from "./../../../components/ProgramsCard/ProgramsCard";
-import Image from "./../../../assets/svgs/image 9.svg";
+//sanity client
 import client from "../../../client";
+//image
+import Image from "./../../../assets/svgs/image 9.svg";
 
 type ProgramCardProps = {
   title: string;
@@ -37,14 +42,18 @@ const OurProgram = () => {
         console.log(data);
         setLoading(false);
       })
-      .catch(console.log);
+      .catch(() => {
+        setLoading(false);
+      });
   }, []);
 
   console.log(posts);
 
   return loading ? (
-    <p>Loading</p>
-  ) : (
+    <div className="my-60">
+      <p className="text-center text-3xl">Loading...</p>
+    </div>
+  ) : posts.length !== 0 ? (
     <div className="md:px-20 py-44">
       <Container fluid>
         {posts.map(
@@ -59,6 +68,21 @@ const OurProgram = () => {
           )
         )}
       </Container>
+    </div>
+  ) : (
+    <div className="my-60">
+      <motion.p
+        className="text-center text-3xl"
+        initial={{
+          opacity: 0,
+        }}
+        animate={{
+          opacity: 1,
+        }}
+        transition={{ duration: 5 }}
+      >
+        Something Went Wrong...
+      </motion.p>
     </div>
   );
 };
